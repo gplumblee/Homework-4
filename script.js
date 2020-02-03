c73d6d120170a4c8321608024114e2a89b1b4e4e
Greg Plumblee <gplumblee@hotmail.com>
2020-02-02 18:32:38 -0800

    updated homework a bit. Still struggling

--- script.js
+++ script.js
@@ -8,31 +8,71 @@
 //     go to next question
 //     after last question, stop the timer
 // If timer reaches 0 before questions are answer, display "You Lost!" message.
-let questions = [
-  "Commonly used data types DO NOT include:",
-  "The condition in an if/else statement is enclosed within_____.",
-  "Arrays in JavaScript can be used to store_____.",
-  "String values must be enclosed within _____ when being assign to variables.",
-  "A useful tool used during development and debugging for printing content to the debugger is:"
+
+let mainQuestions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: [
+      "1. string", 
+      "2. booleans", 
+      "3. alerts", 
+      "4. square"
+    ],
+    answer: 2
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within_____.",
+    choices: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets"
+    ],
+    answer: 2
+  },
+  {
+    question: "Arrays in JavaScript can be used to store_____.",
+    choices: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above"
+    ],
+    answer: 3
+  },
+  {
+    question:
+      "String values must be enclosed within _____ when being assign to variables.",
+    choices: [
+      "1. commas", 
+      "2. curly brackets", 
+      "3. quotes", 
+      "4. parentheses"
+    ],
+    answer: 2
+  },
+  {
+    question:
+      "A useful tool used during development and debugging for printing content to the debugger is:",
+    choices: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log"
+    ],
+    answer: 3
+  }
 ];
-let multChoice = [
-  ["1. string", "2. booleans", "3. alerts", "4. numbers"],
-  ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-  ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-  ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-  ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"]
-];
-let corrAnswrs = ["a3", "a3", "a4", "a3", "a4"];
 
 let counter = 60;
+let timerDiv = document.getElementById("timer");
+timerDiv.textContent = "Timer: " + counter;
 
 function startTimer() {
-  let handle = setInterval(function() {
-    let timerDiv = document.getElementById("timer");
-    timerDiv.textContent = counter;
+  let clock = setInterval(function() {
     counter--;
     if (counter === 0) {
-      clearInterval(handle);
+      clearInterval(clock);
     }
   }, 1000);
 }
@@ -40,13 +80,13 @@ function startTimer() {
 let start = document.getElementById("start");
 start.addEventListener("click", function() {
   startTimer();
-  askQuestion();
+  for (let i = 0; i < mainQuestions.length; i++) {
+    askQuestion(i);
+  }
 });
 
 function askQuestion() {
-  let i = 0;
-  currentQuestion[i];
-  let currentAnswr = document.getElementById(corrAnswrs[i]);
+  currentQuestion(i);
   if (!currentAnswr.addEventListener("click")) {
     counter -= 10;
   }
@@ -55,15 +95,17 @@ function askQuestion() {
 // Create a button for each answer (four buttons)
 
 function currentQuestion(index) {
-  document.getElementById("question").textContent = questions[index];
-  document.getElementById("a1").textContent = multChoice[index][0];
-  document.getElementById("a2").textContent = multChoice[index][1];
-  document.getElementById("a3").textContent = multChoice[index][2];
-  document.getElementById("a4").textContent = multChoice[index][3];
+  document.getElementById("question").textContent = mainQuestions[index].question;
+  document.getElementById("choice1").textContent = mainQuestions[index].choices[0];
+  document.getElementById("choice2").textContent = mainQuestions[index].choices[1];
+  document.getElementById("choice3").textContent = mainQuestions[index].choices[2];
+  document.getElementById("choice4").textContent = mainQuestions[index].choices[3];
 }
 // While counter is greater than 0:
 //   Ask current question starting with index 0
 //   When answer button is clicked:
 //     if correct, add 1 to current question
 //     if incorrect, subtract 10 from counter and repeat loop
-//   When all question are asked, stop timer and display score.
+//   When all questions are asked, stop timer and display score.
+
+// see Week 02/Day 05/Activity 18 for seeing what button user selects
---
+++ scriptx.js
@@ -0,0 +1,213 @@
+let pageTitle = document.getElementById('action-title');
+let startQuizButton = document.getElementById('start-timer');
+let timeRemainingField = document.getElementById('time-remaining');
+let displayHighScoresButton = document.getElementById('high-scores');
+let quizArea = document.getElementById('quiz-area');
+let timeDisplayInterval = 1000; // 1s in ms
+let maxTimeout = 30000; // in ms
+let questionDiv = document.createElement("div");
+let questionIndex = 0;
+let answerDiv = document.createElement("div");
+answerDiv.setAttribute("class", "row answer-div");
+answerDiv.setAttribute("style", "display: inline-block");
+let statusDiv = document.createElement("div");
+statusDiv.setAttribute("class", "row text-muted font-italic mt-1 border-top");
+let choiceButtons = [];
+let countdown = null;
+let submitButton = document.createElement("input");
+let quizTime = 0;
+let finalScore = 0;
+
+
+
+startQuizButton.addEventListener('click', () => {
+  event.preventDefault();
+  let t = maxTimeout/1000; // initial time
+  startTimer(t);
+  askQuestions();
+});
+
+displayHighScoresButton.addEventListener('click', () => {
+  event.preventDefault();
+  if(pageTitle.textContent != 'High Scores') {
+    highScoresElements();
+  }
+});
+
+submitButton.addEventListener('click', () => {
+  event.preventDefault();
+  let userScore = document.getElementById("initials").value + " - " + finalScore;
+  if(!localStorage.getItem('scoreArray')) {
+    let scores = [userScore];
+    localStorage.setItem('scoreArray', JSON.stringify(scores));
+  } else {
+    let scores = JSON.parse(localStorage.getItem("scoreArray"));
+    scores.push(userScore);
+    localStorage.setItem('scoreArray', JSON.stringify(scores));
+  }
+
+  answerDiv.remove();
+  statusDiv.remove();
+  highScoresElements();
+});
+
+function highScoresElements() {
+  event.preventDefault();
+  if (choiceButtons) {
+    choiceButtons.forEach(element => {
+    element.remove();
+    });
+  };
+
+  pageTitle.textContent = 'High Scores'
+  startQuizButton.style.visibility = "hidden";
+  // create the score list elements
+  let scoreList = document.createElement("ol");
+  scoreList.setAttribute("id", "scores-list");
+  quizArea.appendChild(scoreList);
+  let scores = JSON.parse(localStorage.getItem("scoreArray"));
+    scores.sort((a, b) => {
+      return parseInt(b.slice(b.length - 2).trim()) - parseInt(a.slice(a.length - 2).trim());
+  });
+  // list the scores
+  scores.forEach((item) => {
+    let userScore = document.createElement("li");
+    userScore.textContent = item;
+    scoreList.appendChild(userScore);
+  });
+  
+  let buttonDiv = document.createElement("div");
+  buttonDiv.setAttribute("id", "score-list-buttons")
+  let btn = document.createElement("button");
+  btn.textContent = "Go Back"
+  btn.setAttribute("class", "btn btn-info");
+  btn.setAttribute("style", "display: block; margin: 3px");
+  btn.addEventListener('click', () => {
+    let list = document.getElementById("scores-list");
+    if(list) {
+      list.remove();
+    }
+    let scoreButtons = document.getElementById("score-list-buttons");
+    scoreButtons.remove();
+    resetGame();
+  });
+  buttonDiv.appendChild(btn);
+  btn = document.createElement("button");
+  btn.textContent = "Clear High Scores"
+  btn.setAttribute("class", "btn btn-info");
+  btn.setAttribute("style", "margin: 3px");
+  btn.addEventListener('click', () => {
+    localStorage.clear();
+    scoreList.remove();
+  });
+  buttonDiv.appendChild(btn);
+  quizArea.appendChild(buttonDiv);
+};
+
+// function updateScoreList() {
+//   for(let i = 0; i < scoreArray.length; ++i) {
+//     let userScore = document.createElement("li");
+//     userScore.textContent = scoreArray[i];
+//     scoreList.appendChild(userScore);
+//   };
+// };
+
+function startTimer(t) { // this works
+  // this does the second countdown
+  quizTime = t;
+  countdown = setInterval(function() {
+    quizTime -= 1;
+    timeRemainingField.textContent = quizTime;
+    if(quizTime <= 0) {
+      clearInterval(countdown);
+      endQuiz(0);
+    };
+  }, 1000);
+};
+
+function stopTimer() { 
+  clearInterval(countdown);
+  // TODO: might need to also zero out the timer
+};
+
+function dockTime(sec) {
+  quizTime -= sec;
+};
+
+function resetGame() {
+  location.reload();
+}
+
+function askQuestions() {
+  startQuizButton.style.visibility = "hidden";
+  if(questionIndex === questions.length - 1) {
+    return;
+  };
+  displayQuestion(questionIndex);
+};
+
+function displayQuestion(questionIndex) {
+  pageTitle.textContent = questions[questionIndex].title;
+  quizArea.appendChild(answerDiv);
+  quizArea.appendChild(statusDiv);
+  // the first time through, create the choice buttons with no text in them
+  if(choiceButtons.length === 0) {
+    for(let i = 0; i < 4; ++i){
+      choiceButtons[i] = document.createElement("button");
+      answerDiv.appendChild(choiceButtons[i]);
+      choiceButtons[i].setAttribute("class", "btn btn-info")
+      choiceButtons[i].setAttribute("style", "display: block; margin: 3px")
+      choiceButtons[i].addEventListener('click', () => {
+        event.preventDefault();
+        if(questions[questionIndex].answer === choiceButtons[i].textContent) {
+          statusDiv.textContent = "CORRECT!";
+        } else {
+          statusDiv.textContent = "WRONG!";
+          dockTime(10);
+        };
+        questionIndex ++;
+        if(questionIndex < questions.length) {
+          displayQuestion(questionIndex);
+        } else {
+          
+          quizTime = quizTime < 0 ? 0 : quizTime;
+          endQuiz(quizTime);
+          finalScore = quizTime;
+        };
+      });
+    };
+  };
+  // add the text to the buttons
+  for(let i = 0; i < 4; ++i) {
+    choiceButtons[i].textContent = questions[questionIndex].choices[i];
+  };
+}
+
+function endQuiz(score) {
+  let scoreReport = document.createElement("div");
+  let enterInitials = document.createElement("form");
+
+  stopTimer();
+  timeRemainingField.textContent = 0;
+  pageTitle.textContent = 'All Done!';
+
+  choiceButtons.forEach(element => {
+    element.remove();
+  });
+  
+  scoreReport.textContent = "Your final score is " + score;
+  answerDiv.appendChild(scoreReport);
+  
+  enterInitials.innerHTML = "\<div class=\"form-group\"\>Enter your initials: <input id=\"initials\" type=\"text\" name=\"intials: \">\<\/div\>";
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("value", "submit");
+  submitButton.setAttribute("class", "btn btn-success");
+  enterInitials.appendChild(submitButton);
+  answerDiv.appendChild(enterInitials);
+}
+
+function timeFormat(milliseconds) {
+  let min = Math.floor(milliseconds / 60000);
+  let sec = Math.floor(milliseconds - min * 60000) / 1000
+  return min + ':' + sec;
+}
